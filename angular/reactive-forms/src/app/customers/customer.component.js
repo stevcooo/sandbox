67188e846b03239eacb0be0075d9213153bf07e6
Stevan Kostoski <stevcooo@live.com>
2017-11-10 16:01:31 +0100

    ...

--- angular/reactive-forms/src/app/customers/customer.component.js
+++ angular/reactive-forms/src/app/customers/customer.component.js
@@ -5,15 +5,57 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
 var customer_1 = require("./customer");
+function ratingRange(min, max) {
+    return function (c) {
+        if (c.value != undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return { 'range': true };
+        }
+        ;
+        return null;
+    };
+}
 var CustomerComponent = (function () {
-    function CustomerComponent() {
+    function CustomerComponent(fb) {
+        this.fb = fb;
         this.customer = new customer_1.Customer();
     }
-    CustomerComponent.prototype.save = function (customerForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    CustomerComponent.prototype.ngOnInit = function () {
+        this.customerForm = this.fb.group({
+            firstName: ['', [forms_1.Validators.required, forms_1.Validators.minLength(3)]],
+            lastName: ['', [forms_1.Validators.required, forms_1.Validators.maxLength(50)]],
+            email: ['', [forms_1.Validators.required, forms_1.Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+")]],
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1, 5)],
+            sendCatalog: true
+        });
+    };
+    CustomerComponent.prototype.populateTestData = function () {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            email: 'stevan@kostoski.com',
+            sendCatalog: false
+        });
+    };
+    CustomerComponent.prototype.save = function () {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    };
+    CustomerComponent.prototype.setNotification = function (nofifyVia) {
+        var phoneControl = this.customerForm.get('phone');
+        if (nofifyVia === 'text') {
+            phoneControl.setValidators(forms_1.Validators.required);
+        }
+        else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     };
     return CustomerComponent;
 }());
@@ -21,7 +63,8 @@ CustomerComponent = __decorate([
     core_1.Component({
         selector: 'my-signup',
         templateUrl: './app/customers/customer.component.html'
-    })
+    }),
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
 ], CustomerComponent);
 exports.CustomerComponent = CustomerComponent;
 //# sourceMappingURL=customer.component.js.map
--- angular/reactive-forms/src/app/customers/customer.component.ts
+++ angular/reactive-forms/src/app/customers/customer.component.ts
@@ -1,17 +1,63 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators,  AbstractControl, ValidatorFn} from '@angular/forms';
 
 import { Customer } from './customer';
+
+function ratingRange(min: number, max: number) : ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean} | null =>{
+        if(c.value!=undefined && (isNaN(c.value) || c.value<min|| c.value > max)){
+            return {'range': true};
+        };
+        return null;
+    };
+}
+
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent  implements OnInit{
+    
+    customerForm : FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    constructor(private fb: FormBuilder){
+
+    }
+
+    ngOnInit(): void {
+        this.customerForm = this.fb.group({
+            firstName: ['',[Validators.required, Validators.minLength(3)]],
+            lastName: ['',[Validators.required, Validators.maxLength(50)]],
+            email:['',[Validators.required, Validators.pattern("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+")]],
+            phone:'',
+            notification:'email',
+            rating:['', ratingRange(1,5)],
+            sendCatalog: true
+        });
+    }
+
+    populateTestData() : void{
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            email: 'stevan@kostoski.com',
+            sendCatalog: false
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(nofifyVia: string): void{
+        const phoneControl = this.customerForm.get('phone');
+        if(nofifyVia === 'text'){
+            phoneControl.setValidators(Validators.required);
+        }else{
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
  }
